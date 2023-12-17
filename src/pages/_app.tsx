d29e2fb69220f3c0c6e7f1040bb2efcd6a851efb
Pedro Souza <pedrops244@hotmail.com>
2023-12-17 12:15:05 -0300

    configs for styled-components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '@/styles/global-styles';
+import { theme } from '@/styles/theme';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
