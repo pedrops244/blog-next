7fb5696dedb5ae8f5cb205f5a47e11c9b6d0b719
Pedro Souza <pedrops244@hotmail.com>
2023-12-18 19:02:48 -0300

    separating the components and your logics in each files

---
+++ src/config/app-config.ts
@@ -0,0 +1,4 @@
+export const API_URL = 'https://blog-strapi-m768.onrender.com/api';
+export const POSTS_URL = `${API_URL}/posts?populate=*`;
+
+export const SITE_NAME = 'Pedro Souza';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,18 @@
 import { GetStaticProps } from 'next';
 import { PostData } from '../domain/posts/post';
+import { getAllPosts } from '@/data/posts/get-all-posts';
+import HomePage from '@/containers/HomePage';
 
-const getPosts = async (): Promise<PostData[]> => {
-  const posts = await fetch(
-    'https://blog-strapi-m768.onrender.com/api/posts?populate=*',
-  );
-  const jsonPosts = await posts.json();
-  return jsonPosts.data;
-};
 export type HomeProps = {
   posts: PostData[];
 };
 
 export default function Home({ posts }: HomeProps) {
-  return (
-    <div>
-      {posts.map((post) => (
-        <h2 key={post.id}>{post.attributes.title}</h2>
-      ))}
-    </div>
-  );
+  return <HomePage posts={posts} />;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getPosts();
+  const posts = await getAllPosts();
 
   return {
     props: { posts },
