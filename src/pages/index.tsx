f34a8136083f3aa5b048fe1dc200054ba4f19937
Pedro Souza <pedrops244@hotmail.com>
2023-12-18 18:20:23 -0300

    rendering datas of api to strapi, examples only

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <h1>Oi</h1>;
+import { GetStaticProps } from 'next';
+import { PostData } from '../domain/posts/post';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch(
+    'https://blog-strapi-m768.onrender.com/api/posts?populate=*',
+  );
+  const jsonPosts = await posts.json();
+  return jsonPosts.data;
+};
+export type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.id}>{post.attributes.title}</h2>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+
+  return {
+    props: { posts },
+  };
+};
