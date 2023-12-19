6ed257e7bad5e54939edd945102b8d2824589fc0
Pedro Souza <pedrops244@hotmail.com>
2023-12-19 18:09:58 -0300

    creating header component

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,15 +1,19 @@
 import { PostData } from '@/domain/posts/post';
 import { Container } from './styles';
+import { Header } from '@/components/Header';
 
 export type HomePageProps = {
   posts: PostData[];
 };
 export default function HomePage({ posts }: HomePageProps) {
   return (
-    <Container>
-      {posts.map((post) => (
-        <h2 key={post.id}>{post.attributes.title}</h2>
-      ))}
-    </Container>
+    <>
+      <Header />
+      <Container>
+        {posts.map((post) => (
+          <h2 key={post.id}>{post.attributes.title}</h2>
+        ))}
+      </Container>
+    </>
   );
 }
