58abb04327da40b449f511ff054388d81e162c97
Pedro Souza <pedrops244@hotmail.com>
2023-12-21 21:33:12 -0300

    create a iteration for images reactive api and stylization

--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -2,18 +2,25 @@ import { PostData } from '@/domain/posts/post';
 import { Container } from './styles';
 import { Header } from '@/components/Header';
 import { MainContainer } from '@/components/MainContainer';
+import { PostCard } from '@/components/PostCard';
 
 export type HomePageProps = {
   posts: PostData[];
 };
 export default function HomePage({ posts }: HomePageProps) {
+  console.log(posts.map((post) => post.attributes.cover.data.attributes.url));
   return (
     <>
       <Header />
       <MainContainer>
         <Container>
           {posts.map((post) => (
-            <h2 key={post.id}>{post.attributes.title}</h2>
+            <PostCard
+              key={post.id}
+              cover={post.attributes.cover.data.attributes.url}
+              slug={post.attributes.slug}
+              title={post.attributes.title}
+            />
           ))}
         </Container>
       </MainContainer>
