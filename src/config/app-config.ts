4a12b24e8ad94f79728fe77ffa5650379a5a09a2
Pedro Souza <pedrops244@hotmail.com>
2023-12-25 10:26:06 -0300

    add site_url on configs

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,3 +2,4 @@ export const API_URL = 'https://blog-strapi-m768.onrender.com/api';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'My blog for everything';
+export const SITE_URL = 'https://blog-next-pedr1n.netlify.app/';
